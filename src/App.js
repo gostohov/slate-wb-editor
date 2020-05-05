e341e1ea795d96c6692b205ba8cdf618b0fb79ac
Gostohov Zurab <gostohov@gmail.com>
2020-05-05 19:52:02 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toolbar from './Toolbar/Toolbar';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Toolbar />
+      <Sidebar />
+      Non excepteur est dolore qui occaecat labore ad tempor commodo tempor exercitation nulla nisi consequat. Nisi cupidatat cillum qui sit esse excepteur voluptate dolor minim dolore minim adipisicing. Qui elit duis dolor aliquip deserunt proident sint nulla est. Sunt laborum ea excepteur adipisicing mollit nostrud quis ea elit exercitation eiusmod. Irure deserunt eiusmod veniam elit est est sint elit est. Voluptate culpa commodo labore excepteur velit. Ea do nulla laborum enim velit.
+
+Laboris deserunt minim officia exercitation velit dolor laborum incididunt sunt ad aliquip. Eiusmod id dolor exercitation est ullamco esse magna nulla magna nostrud reprehenderit minim nostrud. Eiusmod ullamco nostrud proident ipsum culpa pariatur quis ut laborum. Consequat culpa labore dolor cillum tempor ea ea id reprehenderit fugiat laboris ad. Ea consectetur velit veniam deserunt magna ad.
+
+Non pariatur labore Lorem tempor commodo pariatur esse et Lorem consequat cupidatat non. Occaecat consectetur sint in anim laborum eiusmod fugiat anim mollit reprehenderit commodo. Velit culpa pariatur incididunt sunt minim eiusmod dolore enim id. Reprehenderit sit veniam anim occaecat aute in. Sint pariatur irure anim aute proident eu non. Velit consequat eiusmod ad do aliqua eiusmod laboris.
+
+Amet sit ut aliqua et aute velit nisi occaecat reprehenderit ea. Officia amet aute nostrud ullamco aliqua ex amet excepteur. Do laborum non eiusmod elit laborum officia fugiat nostrud. Laborum sit aute proident cupidatat sit consequat mollit do in do velit velit laboris. Ullamco sint quis consectetur irure ut qui dolor labore aliquip reprehenderit.
+
+Minim ad velit qui amet incididunt ea culpa. Duis consequat officia et occaecat minim ipsum. Ullamco culpa aliquip magna velit cupidatat aliqua eiusmod. Dolor nisi esse adipisicing ut magna qui qui eu officia deserunt cupidatat non. Fugiat anim velit labore nisi quis irure sunt cupidatat excepteur ipsum reprehenderit adipisicing.
+
+Anim dolore Lorem voluptate id do labore voluptate cupidatat do enim cupidatat proident qui ullamco. Cupidatat exercitation esse proident occaecat Lorem velit. Mollit enim Lorem consequat incididunt aute proident ipsum ex pariatur fugiat eiusmod esse do. Proident sunt occaecat non sit duis.
+
+Elit eu velit laboris exercitation reprehenderit ut veniam nulla consequat et duis esse. Eiusmod cillum tempor dolore laborum fugiat reprehenderit pariatur aliquip minim qui nostrud est. Nostrud ipsum pariatur cillum adipisicing exercitation consequat fugiat occaecat. Tempor nisi Lorem id do tempor commodo consequat ad pariatur tempor dolor non ea.
+
+Consectetur velit sint enim adipisicing qui ipsum tempor. Do esse esse anim eu ad est in do ipsum do commodo. Dolore ipsum aute anim anim esse commodo magna aliqua. Elit do culpa fugiat voluptate sint qui non. Cillum non sint irure adipisicing aliquip est minim nostrud veniam quis. Dolor minim ad ad adipisicing.
+
+Do incididunt ad adipisicing laborum laboris. Minim minim Lorem occaecat do enim amet sunt ad esse deserunt aliqua eiusmod. Reprehenderit nisi pariatur aute ex occaecat ex est. Pariatur esse occaecat commodo non amet voluptate commodo culpa occaecat.
+
+Minim eu excepteur proident dolor magna labore sint sunt aute enim reprehenderit nostrud sit officia. Non ea do proident reprehenderit elit tempor aliqua esse amet nostrud et et. Sunt excepteur laborum incididunt dolor aute ea pariatur ea irure id est aliquip ut qui. Commodo irure fugiat velit irure labore officia excepteur ad reprehenderit ipsum veniam.
     </div>
   );
 }
---
+++ src/Toolbar/Toolbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Toolbar.css'
+
+const Toolbar = () => {
+  return (
+    <div className="toolbar-container">Toolbar</div>
+  )
+}
+
+export default Toolbar
